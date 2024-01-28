aece3f5b495ef52e33436fa238837c028e2ae41b
Mateus Junges <mateus@junges.dev>
2024-01-27 21:34:58 -0300

    Update migration to follow most recent laravel version

--- database/migrations/2020_01_29_162459_create_invites_table.php
+++ database/migrations/2020_01_29_162459_create_invites_table.php
@@ -4,14 +4,9 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateInvitesTable extends Migration
+return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
+    public function up() :void
     {
         $table_name = config('invite-codes.tables.invites_table', 'invites');
 
@@ -27,13 +22,8 @@ class CreateInvitesTable extends Migration
         });
     }
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
+    public function down(): void
     {
         Schema::dropIfExists(config('invite-codes.tables.invites_table', 'invites'));
     }
-}
+};
